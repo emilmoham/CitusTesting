470b5d9701736ff48d55a39e30bb986fcb07a103
Emil Mohammed <emohammed@prodigytel.com>
2024-05-22 16:51:30 -0400

    scripts to see how tables are distributed

--- SQL/MultipleShards/ObserveSharding.sql
+++ SQL/MultipleShards/ObserveSharding.sql
@@ -1,8 +1,9 @@
+select * from pg_dist_shard;
+
 SELECT 
 	shard.logicalrelid,
 	shard.shardid,
-	node.nodename,
-    node.nodeport
+	node.nodename
 from pg_dist_shard shard
 join pg_dist_placement placement
 	on placement.shardid = shard.shardid
