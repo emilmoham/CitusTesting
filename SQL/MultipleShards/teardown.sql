bafaf1e75dd40c37dd27fabdccce5931cd23a98a
Emil Mohammed <emohammed@prodigytel.com>
2024-05-22 16:49:40 -0400

    scripts to remove tables if needed

--- SQL/MultipleShards/teardown.sql
+++ SQL/MultipleShards/teardown.sql
@@ -1,9 +1,18 @@
---select undistribute_table('entries', cascade_via_foreign_keys=>true);
+DELETE FROM accounting.public.entries;
+
+DELETE FROM accounting.public.transactions;
+
+DELETE FROM accounting.public.accounts;
+
+DELETE FROM accounting.public.facilities;
+
+
+select undistribute_table('entries', cascade_via_foreign_keys=>true);
 --select undistribute_table('transactions', cascade_via_foreign_keys=>true);
 --select undistribute_table('accounts');
 --select undistribute_table('facilities');
 
---DROP TABLE entries;
---DROP TABLE transactions;
---DROP TABLE accounts;
---DROP TABLE facilities;
+DROP TABLE entries;
+DROP TABLE transactions;
+DROP TABLE accounts;
+DROP TABLE facilities;
