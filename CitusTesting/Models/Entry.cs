ce72280c892196c334fab704dd2912ac78b9536b
Emil Mohammed <emohammed@prodigytel.com>
2024-05-22 14:50:02 -0400

    Added facility column

--- CitusTesting/Models/Entry.cs
+++ CitusTesting/Models/Entry.cs
@@ -3,6 +3,7 @@
     public class Entry
     {
         public long Id { get; set; }
+        public int FacilityId { get; set; }
         public long TransactionId { get; set; }
         public int AccountId { get; set; }
         public int Credit { get; set; }
