6e36138709364b038cd6752d9fa415118702fdfc
Emil Mohammed <emohammed@prodigytel.com>
2024-05-22 18:38:43 -0400

    added query to find what node a shards data is stored on

--- SQL/MultipleShards/ObserveSharding.sql
+++ SQL/MultipleShards/ObserveSharding.sql
@@ -9,3 +9,29 @@ join pg_dist_placement placement
 	on placement.shardid = shard.shardid
 join pg_dist_node node
 	on placement.groupid = node.groupid and node.noderole = 'primary'::noderole;
+
+
+select * from facilities f ;
+
+with constants(facility_id) as (
+values (8))
+select
+	shardid,
+	shardstate,
+	shardlength,
+	nodename,
+	nodeport,
+	placementid
+from
+	pg_dist_placement as placement,
+	pg_dist_node as node,
+	constants as c
+where
+	placement.groupid = node.groupid
+	and node.noderole = 'primary'
+   AND (
+   		   shardid = (SELECT get_shard_id_for_distribution_column('facilities', c.facility_id))
+		or shardid = (SELECT get_shard_id_for_distribution_column('accounts', c.facility_id))
+		or shardid = (SELECT get_shard_id_for_distribution_column('transactions', c.facility_id))
+		or shardid = (SELECT get_shard_id_for_distribution_column('entries', c.facility_id))
+   );
