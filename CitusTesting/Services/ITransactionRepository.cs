c08a0abc54f529a9a85e45ea13c8e71853430a9d
Emil Mohammed <emilmoham@gmail.com>
2024-05-25 18:10:06 -0400

    Transactions service

---
+++ CitusTesting/Services/ITransactionRepository.cs
@@ -0,0 +1,14 @@
+﻿namespace CitusTesting.Services
+{
+    public interface ITransactionRepository
+    {
+        Task<IEnumerable<Entities.Transaction>> GetTransactionsAsync(int? facilityId);
+        IAsyncEnumerable<Entities.Transaction> GetTransactionsAsAsyncEnumerable(int? facilityId);
+
+        Task<Entities.Transaction?> GetTransactionAsync(int id);
+
+        void AddTransaction(Entities.Transaction transactionToAdd);
+
+        Task<bool> SaveChangesAsync();
+    }
+}
---
+++ CitusTesting/Services/TransactionRepository.cs
@@ -0,0 +1,45 @@
+﻿using CitusTesting.DbContexts;
+using Microsoft.EntityFrameworkCore;
+
+namespace CitusTesting.Services
+{
+    public class TransactionRepository : ITransactionRepository
+    {
+        private AccountingContext _context;
+
+        public TransactionRepository(AccountingContext context)
+        {
+            _context = context;
+        }
+
+        public void AddTransaction(Entities.Transaction transactionToAdd)
+        {
+            if (transactionToAdd == null) throw new ArgumentNullException(nameof(transactionToAdd));
+
+            _context.Transactions.Add(transactionToAdd);
+        }
+
+        public async Task<IEnumerable<Entities.Transaction>> GetTransactionsAsync(int? facilityId) {
+            if (facilityId != null)
+                return await _context.Transactions.Where(t => t.FacilityId == facilityId).ToListAsync(); 
+            return await _context.Transactions.ToListAsync();
+        }
+
+        public IAsyncEnumerable<Entities.Transaction> GetTransactionsAsAsyncEnumerable(int? facilityId)
+        {
+            if (facilityId != null)
+                return _context.Transactions.Where(t => t.FacilityId == facilityId).AsAsyncEnumerable();
+            return _context.Transactions.AsAsyncEnumerable();
+        }
+
+        public async Task<Entities.Transaction?> GetTransactionAsync(int id)
+        {
+            return await _context.Transactions.FirstOrDefaultAsync(t => t.Id == id);
+        }
+
+        public async Task<bool> SaveChangesAsync()
+        {
+            return (await _context.SaveChangesAsync() > 0);
+        }
+    }
+}
