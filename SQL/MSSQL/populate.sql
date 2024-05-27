37efe122bb6375e4da84000aa61dbbd9d00e2939
Emil Mohammed <emilmoham@gmail.com>
2024-05-27 15:02:06 -0400

    Scripts to populate database before testing

--- SQL/MSSQL/populate.sql
+++ SQL/MSSQL/populate.sql
@@ -1,12 +1,32 @@
-INSERT INTO accounting.dbo.facilities (Name) values (
-	'Boston',
-	'Cheyenne',
-	'Des Moines',
-	'Jackson',
-	'New York',
-	'Pheonix',
-	'Philadelphia',
-	'Sacramento',
-	'Salem',
-	'Trenton'
+INSERT INTO accounting.dbo.facilities (Name) values
+	('Boston'),
+	('Cheyenne'),
+	('Des Moines'),
+	('Jackson'),
+	('New York'),
+	('Pheonix'),
+	('Philadelphia'),
+	('Sacramento'),
+	('Salem'),
+	('Trenton')
+
+CREATE TABLE #account_temp(
+	Name varchar(50),
+	Number int,
+	Type bit,
 )
+
+--SELECT * FROM #account_temp as a right join accounting.dbo.facilities as f on 1=1;
+
+INSERT INTO #account_temp 
+	(Name, Number, [Type]) 
+VALUES 
+	('Assets', 1000, 0),
+	('Liabilities', 2000, 1),
+	('Equity', 3000, 1),
+	('Revenue', 4000, 1),
+	('Expenses', 5000, 0)
+
+INSERT INTO accounting.dbo.accounts	
+	(Name, Number, [Type], FacilityId)
+SELECT a.Name, a.Number, a.[Type], f.id FROM #account_temp as a right join [accounting].[dbo].[facilities] as f on 1=1
---
+++ SQL/SingleShard/populate.sql
@@ -0,0 +1,32 @@
+INSERT INTO facilities (Name) values
+	('Boston'),
+	('Cheyenne'),
+	('Des Moines'),
+	('Jackson'),
+	('New York'),
+	('Pheonix'),
+	('Philadelphia'),
+	('Sacramento'),
+	('Salem'),
+	('Trenton');
+	
+CREATE TEMPORARY TABLE account_temp(
+	name varchar(50),
+	number int,
+	type boolean
+);
+
+INSERT INTO account_temp 
+	(name, number, type) 
+VALUES 
+	('Assets', 1000, false),
+	('Liabilities', 2000, true),
+	('Equity', 3000, true),
+	('Revenue', 4000, true),
+	('Expenses', 5000, false)
+
+--SELECT * FROM account_temp as a right join facilities as f on 1=1;
+
+INSERT INTO accounts	
+	(name, number, type, facility_id)
+SELECT a.name, a.number, a.type, f.id FROM account_temp as a right join facilities as f on 1=1
